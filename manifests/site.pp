9c627a891ae65d38cf2a6cb63540de4dd213fb05
Peter Souter <petems@users.noreply.github.com>
2016-11-08 14:41:26 +0000

    Add node definition for my node

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,6 +20,13 @@
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
+node 'instructorwin.puppetlabs.vm' {
+  notify {'I will get matched here!':}
+}
+
+node /.+\.puppetlabs\.vm/ {
+  notify {'I will get matched here!':}
+}
 
 node default {
   # This is where you can declare classes for all nodes.
